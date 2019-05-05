63c2d3c97205587274deb97562cd0102e72c2608
hello-doctor <jetlee1906@163.com>
2019-05-05 19:55:38 +0800

    增加推荐模块

---
+++ src/pages/home/components/MyRecommend.vue
@@ -0,0 +1,91 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item" v-for="item in recommendList" :key="item.id">
+        <img class="item-img"
+             :src="item.imgUrl"/>
+        <div class="item-info border-bottom">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyRecommend',
+  data () {
+    return {
+      recommendList: [
+        {
+          id: '1',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1904/1e/1e51b5ea32ee8033a3.water.jpg_200x200_7750aa0b.jpg',
+          title: '梵净山',
+          desc: '国家5A级风景名胜区'
+        }, {
+          id: '2',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1904/1e/1e51b5ea32ee8033a3.water.jpg_200x200_7750aa0b.jpg',
+          title: '梵净山',
+          desc: '国家5A级风景名胜区'
+        }, {
+          id: '3',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1904/1e/1e51b5ea32ee8033a3.water.jpg_200x200_7750aa0b.jpg',
+          title: '梵净山',
+          desc: '国家5A级风景名胜区'
+        }, {
+          id: '4',
+          imgUrl: 'http://img1.qunarzz.com/sight/p0/1904/1e/1e51b5ea32ee8033a3.water.jpg_200x200_7750aa0b.jpg',
+          title: '梵净山',
+          desc: '国家5A级风景名胜区'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+
+    .item-img
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+
+      .item-button
+        line-height: .44rem
+        margin-top: .16rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/MyWeekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of weekendList"
+        :key="item.id"
+      >
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl" />
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyWeekend',
+  data () {
+    return {
+      weekendList: [
+        {
+          id: '1',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+          title: '极地海洋世界',
+          desc: '很多海洋动物'
+        },
+        {
+          id: '2',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+          title: '极地海洋世界',
+          desc: '很多海洋动物'
+        },
+        {
+          id: '3',
+          imgUrl: 'http://img1.qunarzz.com/sight/source/1811/15/66f14e0fd6fbb.jpg_r_640x214_5d69f21d.jpg',
+          title: '极地海洋世界',
+          desc: '很多海洋动物'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 37.09%
+    .item-img
+      width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
