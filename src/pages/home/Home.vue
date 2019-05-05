8ea9a76e1ac697a3e5aa76281c5056ab9ee46249
hello-doctor <jetlee1906@163.com>
2019-05-05 14:16:18 +0800

    首页轮播效果

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,22 @@
 <template>
   <div>
     <home-header></home-header>
+    <my-swiper></my-swiper>
+    <div>
+      test
+    </div>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import MySwiper from './components/MySwiper'
 
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    MySwiper
   }
 }
 </script>
