66886ffaeb9fa1ae9ae37f7c3783e96cd8b3065f
hello-doctor <jetlee1906@163.com>
2019-05-14 15:26:14 +0800

    当前城市 热门城市 导航 完成了样式

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,18 +2,24 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alpha-beta></city-alpha-beta>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphaBeta from './components/AlphaBeta'
 
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphaBeta
   }
 }
 </script>
